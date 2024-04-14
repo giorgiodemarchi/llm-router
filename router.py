66907bb475b32b4350761c17e4719a6b6e9b9404
giorgiodemarchi <giorgio_demarchi@yahoo.com>
2024-04-14 14:55:00 -0400

    call_model

--- router.py
+++ router.py
@@ -1,3 +1,5 @@
+import os
+from together import Together
 
 class Router:
   models: list
@@ -5,8 +7,18 @@ class Router:
   def _get_optimal_model(query):
     pass
 
-  def request_api(query, model):
-    pass
+  def call_model(query, model):
+    """
+    Takes a model and an input prompt, calls Together.ai and outputs response
+    """
+    client = Together(api_key=os.environ['TOGETHER_API_KEY'])
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": query}],
+    )
+    return response.choices[0].message.content
+
   
   def answer_query(query):
     model = _get_optimal_model(query)
