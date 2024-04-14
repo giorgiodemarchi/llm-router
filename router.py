2956641df356336a6c2c06c53d99dc1f0c962291
benjamrio <benjamrio@yahoo.fr>
2024-04-14 14:43:13 -0400

    init

---
+++ router.py
@@ -0,0 +1,13 @@
+
+class Router:
+  models: list
+
+  def _get_optimal_model(query):
+    pass
+
+  def request_api(query, model):
+    pass
+  
+  def answer_query(query):
+    model = _get_optimal_model(query)
+    pass
